0d7669e1d32c3b56a05ef536315d9be9705fb37c
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:57:29 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz41.cpp
@@ -0,0 +1,15 @@
+﻿/*
+* Пользователь 	вводит текущий год и год его рождения.Посчитать, сколько лет пользователю.
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int date, birthday;
+	cout << "Enter year date:";
+	cin >> date;
+	cout << endl << "Enter year birthday:";
+	cin >> birthday;
+	cout << "Your age " << date - birthday << endl;
+	return 0;
+}
